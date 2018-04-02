4367b493e635609af71d394a1ab7500578b7315a
Aurélien Essig <aurelien.essig@akson.ch>
2018-04-02 09:29:18 +0200

    added new icons and update mobile-config.js file

--- mobile-config.js
+++ mobile-config.js
@@ -1,17 +1,29 @@
 App.icons({
-  iphone_2x: 'resources/icons/iphone_2x.png',
-  iphone_3x: 'resources/icons/iphone_3x.png',
-  ipad: 'resources/icons/ipad.png',
-  ipad_2x: 'resources/icons/ipad_2x.png',
-  ipad_pro: 'resources/icons/ipad_pro.png',
-  ios_settings: 'resources/icons/ios_settings.png',
-  ios_settings_2x: 'resources/icons/ios_settings_2x.png',
-  ios_settings_3x: 'resources/icons/ios_settings_3x.png',
-  ios_spotlight: 'resources/icons/ios_spotlight.png',
-  ios_spotlight_2x: 'resources/icons/ios_spotlight_2x.png',
-  android_mdpi: 'resources/icons/android_mdpi.png',
-  android_hdpi: 'resources/icons/android_hdpi.png',
-  android_xhdpi: 'resources/icons/android_xhdpi.png',
-  android_xxhdpi: 'resources/icons/android_xxhdpi.png',
-  android_xxxhdpi: 'resources/icons/android_xxxhdpi.png',
+  app_store: 'resources/icons/icon-1024.png', // (1024x1024) // Apple App Store
+  iphone_2x: 'resources/icons/icon-60@2x.png', // (120x120) // iPhone 5, SE, 6, 6s, 7, 8
+  iphone_3x: 'resources/icons/icon-60@3x.png', // (180x180) // iPhone 6 Plus, 6s Plus, 7 Plus, 8 Plus, X
+  ipad_2x: 'resources/icons/icon-76@2x.png', // (152x152) // iPad, iPad mini
+  ipad_pro: 'resources/icons/icon-83.5@2x.png', // (167x167) // iPad Pro
+  ios_settings_2x: 'resources/icons/icon-small@2x.png', // (58x58) // iPhone 5, SE, 6, 6s, 7, 8, iPad, mini, Pro
+  ios_settings_3x: 'resources/icons/icon-small@3x.png', // (87x87) // iPhone 6 Plus, 6s Plus, 7 Plus, 8 Plus, X
+  ios_spotlight_2x: 'resources/icons/icon-40@2x.png.png', // (80x80) // iPhone 5, SE, 6, 6s, 7, 8, iPad, mini, Pro
+  ios_spotlight_3x: 'resources/icons/icon-60@2x.png.png', // (120x120) // iPhone 6 Plus, 6s Plus, 7 Plus, 8 Plus, X
+  ios_notification_2x: 'resources/icons/icon-20@2x.png', // (40x40) // iPhone 5, SE, 6, 6s, 7, 8, iPad, mini, Pro
+  ios_notification_3x: 'resources/icons/icon-20@3x.png', // (60x60 // iPhone 6 Plus, 6s Plus, 7 Plus, 8 Plus, X
+  ipad: 'resources/icons/icon-76.png', // (76x76) // Legacy
+  ios_settings: 'resources/icons/icon-small.png', // (29x29) // Legacy
+  ios_spotlight: 'resources/icons/icon-20@2x.png', // (40x40) // Legacy
+  ios_notification: 'resources/icons/icon-20.png', // (20x20) // Legacy
+  iphone_legacy: 'resources/icons/icon.png', // (57x57) // Legacy
+  iphone_legacy_2x: 'resources/icons/icon@2x.png', // (114x114) // Legacy
+  ipad_spotlight_legacy: 'resources/icons/icon-50.png', // (50x50) // Legacy
+  ipad_spotlight_legacy_2x: 'resources/icons/icon-50@2x.png', // (100x100) // Legacy
+  ipad_app_legacy:' resources/icons/icon-72.png', // (72x72) // Legacy
+  ipad_app_legacy_2x: 'resources/icons/icon-72@2x.png', // (144x144) // Legacy
+
+  android_mdpi: 'resources/icons/mipmap-mdpi.png', // (48x48)
+  android_hdpi: 'resources/icons/mipmap-hdpi.png', // (72x72)
+  android_xhdpi: 'resources/icons/mipmap-xhdpi.png', // (96x96)
+  android_xxhdpi: 'resources/icons/mipmap-xxhdpi.png', // (144x144)
+  android_xxxhdpi: 'resources/icons/mipmap-xxxhdpi.png', // (192x192)
 });
